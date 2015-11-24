b2a7bf02c356545204ca0395a78ba3725207f96c
Mohamed Usman <emyu10@gmail.com>
2015-11-24 15:49:18 +0500

    first commit with office work

---
+++ app/src/main/java/com/emyu/android/wordgenerator/GenerateActivity.java
@@ -0,0 +1,18 @@
+package com.emyu.android.wordgenerator;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by emyu on 11/24/15.
+ */
+public class GenerateActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_generate);
+    }
+
+
+}
